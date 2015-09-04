2eb723bf09cedd768141ab5f42cd748075c085ed
Odin Shen <lunming@gmail.com>
2015-09-04 11:56:11 +0800

    Change the daily record from dict to class, implement few method about print/get...

--- SalaryAnalysis.py
+++ SalaryAnalysis.py
@@ -17,9 +17,6 @@ import matplotlib.pyplot as plt
 
 
 logfile = 'SalaryLog.txt'
-'''
-logfile = 'SalaryLogTemp3.txt'
-'''
 
 SalaryDate = []
 UsdSalary = []
@@ -32,7 +29,7 @@ Cny2NtdRate = []
 record_list = []
 
 Cny2Usd_conv_Usd2Ntd = []
-
+'''
 def dailyRecord(SalaryDate, UsdSalary, NtdSalary, Cny2UsdRate, Usd2NtdRate):
     return {
         'SalaryDate': SalaryDate,
@@ -42,15 +39,36 @@ def dailyRecord(SalaryDate, UsdSalary, NtdSalary, Cny2UsdRate, Usd2NtdRate):
         'Usd2NtdRate': Usd2NtdRate,
         'Cny2NtdRate': Cny2NtdRate
     }
+'''
+
+
+class dailyRecord:
+    def __init__(self, SalaryDate, UsdSalary, NtdSalary, Cny2UsdRate, Usd2NtdRate, Cny2NtdRate):
+        self.SalaryDate  = SalaryDate
+        self.UsdSalary   = UsdSalary
+        self.NtdSalary   = NtdSalary
+        self.Cny2UsdRate = Cny2UsdRate
+        self.Usd2NtdRate = Usd2NtdRate
+        self.Cny2NtdRate = Cny2NtdRate
+
+    def getNtdSalary(self):
+        return self.NtdSalary
+
+    def getUsdSalary(self):
+        return self.UsdSalary
+
+    def __str__(self):
+        string = str(self.SalaryDate) + ',\t' + str(self.UsdSalary) + ',\t' + str(self.NtdSalary)
+        return string
 
 with open(logfile, 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     analysis_list = list(spamreader)
 
-print analysis_list
 ''' pass the first line '''
 analysis_list = analysis_list[1:]
 print analysis_list
+
 
 for item in analysis_list:
     SalaryDate.append(item[0])
@@ -61,37 +79,32 @@ for item in analysis_list:
     Cny2NtdRate.append(float(Decimal(item[6][1:])))
     usd2ntd_conv_cny2usd = float(Decimal(item[4][1:])) * float(Decimal(item[5][1:]))
     Cny2Usd_conv_Usd2Ntd.append(usd2ntd_conv_cny2usd)
+
     newRecord = dailyRecord(
         item[0],
         float(Decimal(item[2][1:])),
         float(Decimal(item[3][1:])),
         float(Decimal(item[4][1:])),
-        float(Decimal(item[5][1:]))
+        float(Decimal(item[5][1:])),
+        float(Decimal(item[6][1:]))
     )
     record_list.append(newRecord)
 
 '''
-print SalaryDate
-print UsdSalary
-print NtdSalary
-print Cny2UsdRate
-print Usd2NtdRate
+sorted_NtdSalary_record_list = record_list.sort(key=lambda x: x['NtdSalary'])
 '''
 
-for date in record_list:
-    date = date['SalaryDate']
-    print date
+print sorted_NtdSalary_record_list
 
-for money in record_list:
-    money = money['UsdSalary']
-    print money
+print '\n\nList all record'
+for item in record_list:
+    print item
 
+'''
 print Usd2NtdRate
 print Cny2UsdRate
-'''
-USD2NTD_div_CNY2USD = Usd2NtdRate / Cny2UsdRate
-'''
 print Cny2Usd_conv_Usd2Ntd
+'''
 
 '''plt.plot(UsdSalary)'''
 plt.plot(NtdSalary)
