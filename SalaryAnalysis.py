f75d8a0342fda14396d33c0b829c5d49679ee062
Odin Shen <lunming@gmail.com>
2015-09-07 17:11:30 +0800

    Optimize code, iPython runable

--- SalaryAnalysis.py
+++ SalaryAnalysis.py
@@ -11,6 +11,11 @@ Created on Tue Aug 25 18:09:37 2015
 
 """
 
+'''
+""" Add %matplotlib inline once we want run on iPython """
+%matplotlib inline
+'''
+
 import csv
 import matplotlib.pyplot as plt
 from decimal import *
@@ -18,11 +23,13 @@ from decimal import *
 
 logfile = 'SalaryLog.txt'
 record_list = []
-Cny2Usd_conv_Usd2Ntd = []
 NtdSalaryList = []
 PreLog = []
 PreClass = []
 
+'''
+Cny2Usd_conv_Usd2Ntd = []
+'''
 
 '''
 def dailyRecord(SalaryDate, UsdSalary, NtdSalary, Cny2UsdRate, Usd2NtdRate):
@@ -37,6 +44,7 @@ def dailyRecord(SalaryDate, UsdSalary, NtdSalary, Cny2UsdRate, Usd2NtdRate):
 '''
 
 
+## record class for daily report
 class dailyRecord:
     def __init__(self, SalaryDate, UsdSalary, NtdSalary, Cny2UsdRate, Usd2NtdRate, Cny2NtdRate):
         self.SalaryDate  = SalaryDate
@@ -50,46 +58,60 @@ class dailyRecord:
         string = str(self.SalaryDate) + ',\t' + str(self.UsdSalary) + ',\t' + str(self.NtdSalary)
         return string
 
+
+## Load log from CSV file which produced by CalSalaryExRate.py ##
 with open(logfile, 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     analysis_list = list(spamreader)
 
+
 ## Pass the first line ##
 analysis_list = analysis_list[1:]
+'''
 print analysis_list
+'''
 
+## Sort all log by date ##
 analysis_list = sorted(analysis_list, key=lambda x: x[0])
 
+
+## Create record class and link ##
 for item in analysis_list:
+    '''
     usd2ntd_conv_cny2usd = float(Decimal(item[4][1:])) * float(Decimal(item[5][1:]))
     Cny2Usd_conv_Usd2Ntd.append(usd2ntd_conv_cny2usd)
-    
+    '''
+
     ## Remove duplicate daily log by DATE ##
     if ( item[0] == PreLog ):
         record_list.pop()
     
     ## Create class for ecah of log ##
-    newRecord = dailyRecord(
-        item[0],
-        float(Decimal(item[2][1:])),
-        float(Decimal(item[3][1:])),
-        float(Decimal(item[4][1:])),
-        float(Decimal(item[5][1:])),
-        float(Decimal(item[6][1:]))
-    )
-    record_list.append(newRecord)
+    record_list.append(
+                    dailyRecord(
+                        item[0],
+                        float(Decimal(item[2][1:])),
+                        float(Decimal(item[3][1:])),
+                        float(Decimal(item[4][1:])),
+                        float(Decimal(item[5][1:])),
+                        float(Decimal(item[6][1:]))
+                    )
+                )
     PreLog = item[0]
-    PreClass = newRecord
+'''    PreClass = newRecord   '''
 
+
+## Traverse all class on the record_list ##
 print '\n\nList all record'
-for item in record_list:
-    print item
-    item.NtdSalary
-    item.SalaryDate
-    NtdSalaryList.append(item.NtdSalary)
+for class_num in range(len(record_list)):
+    record_list[class_num].NtdSalary
+    record_list[class_num].SalaryDate
+    NtdSalaryList.append(record_list[class_num].NtdSalary)
+    print(record_list[class_num].NtdSalary)
+
 
 plt.plot(NtdSalaryList)
 
+
 print '\n\nToday\'s record'
 print record_list[len(record_list)-1]
-print 'End Python\n\n'
